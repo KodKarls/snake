6096a61fb8fcfb7f34cca743a732f5b080b62fcc
Carolinii <carolinii.contact@gmail.com>
2024-02-02 14:42:32 +0100

    create the Window class

---
+++ Engine/Window.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+
+#include <SFML/Graphics.hpp>
+
+class Window
+{
+public:
+    Window();
+    Window( const std::string& title, const sf::Vector2u& size );
+    ~Window();
+
+    void update();
+
+    void beginDraw();
+    void draw( sf::Drawable& drawable );
+    void endDraw();
+
+    void toggleFullscreen();
+
+    bool isFullscreen() const;
+    bool isDone() const;
+
+    sf::Vector2u  getWindowSize();
+
+private:
+    void setup( const std::string& title, const sf::Vector2u& size );
+
+    void create();
+    void destroy();
+
+    bool is_done{ false };
+    bool is_fullscreen{ false };
+
+    std::string window_title;
+    sf::Vector2u window_size;
+    sf::RenderWindow window;
+};
